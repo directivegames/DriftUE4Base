3cfe830358814ba258b8b01cf69a5f99c7dc5904
Wang Hao <991060@gmail.com>
2019-06-05 13:01:22 +0800

    Changed the copyright notice for potential issue of using the drift plugin in Kairos.

--- JsonArchive/Private/JsonArchive.cpp
+++ JsonArchive/Private/JsonArchive.cpp
@@ -1,14 +1,4 @@
-/**
-* This file is part of the Drift Unreal Engine Integration.
-*
-* Copyright (C) 2016-2017 Directive Games Limited. All Rights Reserved.
-*
-* Licensed under the MIT License (the "License");
-*
-* You may not use this file except in compliance with the License.
-* You may obtain a copy of the license in the LICENSE file found at the top
-* level directory of this module, and at https://mit-license.org/
-*/
+// Copyright 2015-2019 Directive Games Limited - All Rights Reserved.
 
 #include "JsonArchive.h"
 #include "Json.h"
