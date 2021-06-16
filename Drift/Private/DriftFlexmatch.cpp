ba9a55d984f369cd3b3e24161cd5e328fd7fd98e
Einar Th. Einarsson <einsi@directivegames.com>
2021-06-16 12:45:20 +0000

    Latency measuring and reporting first pass implementation

--- Drift/Private/DriftFlexmatch.cpp
+++ Drift/Private/DriftFlexmatch.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 Directive Games Limited - All Rights Reserved
 
 #include "DriftFlexmatch.h"
-#include "Engine/GameInstance.h"
+
 
 DEFINE_LOG_CATEGORY(LogDriftMatchmaking);
 
-FDriftFlexmatch::FDriftFlexmatch(TSharedPtr<IDriftMessageQueue> MessageQueue)
-	: MessageQueue{MoveTemp(MessageQueue)}
+FDriftFlexmatch::FDriftFlexmatch(TSharedPtr<IDriftMessageQueue> MessageQueue_)
+	: MessageQueue{MoveTemp(MessageQueue_)}
 {
 	MessageQueue->OnMessageQueueMessage(TEXT("matchmaking")).AddRaw(
 		this, &FDriftFlexmatch::HandleMatchmakingEvent);
@@ -27,14 +27,68 @@ void FDriftFlexmatch::SetEndpoint(const FString& MatchmakingUrl)
 	FlexmatchURL = MatchmakingUrl;
 }
 
+void FDriftFlexmatch::Tick( float DeltaTime )
+{
+	if ( DoPings )
+	{
+		TimeToPing -= DeltaTime;
+
+		if (TimeToPing < 0)
+		{
+			ReportLatencies();
+			TimeToPing = PingInterval;
+		}
+	}
+}
+
+bool FDriftFlexmatch::IsTickable() const
+{
+	return true;
+}
+
+TStatId FDriftFlexmatch::GetStatId() const
+{
+	RETURN_QUICK_DECLARE_CYCLE_STAT(FDriftFlexmatch, STATGROUP_Tickables);
+}
+
+void FDriftFlexmatch::ReportLatencies()
+{
+	const auto HttpModule = &FHttpModule::Get();
+	for(auto Region: PingRegions)
+	{
+		const auto Request = HttpModule->CreateRequest();
+		Request->SetVerb("GET");
+		Request->SetURL(FString::Format(*PingUrlTemplate, {Region}));
+		Request->OnProcessRequestComplete().BindLambda(
+		[this, Region](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bConnectedSuccessfully)
+		{
+			if (!bConnectedSuccessfully)
+			{
+				UE_LOG(LogDriftMatchmaking, Error, TEXT("FDriftFlexmatch::ReportLatencies - Failed to connect to '%s'"), *Request->GetURL());
+				return;
+			}
+			JsonValue Payload{rapidjson::kObjectType};
+			JsonArchive::AddMember(Payload, TEXT("region"), *Region);
+			JsonArchive::AddMember(Payload, TEXT("latency_ms"), Request->GetElapsedTime() * 1000);
+			auto PatchRequest = RequestManager->Patch(FlexmatchURL, Payload, HttpStatusCodes::Ok);
+			PatchRequest->OnError.BindLambda([this](ResponseContext& context)
+			{
+				UE_LOG(LogDriftMatchmaking, Error, TEXT("FDriftFlexmatch::ReportLatencies - Failed to report latencies to %s"
+					", Response code %d, error: '%s'"), *FlexmatchURL, context.responseCode, *context.error);
+			});
+			PatchRequest->Dispatch();
+		});
+	}
+}
+
 void FDriftFlexmatch::StartLatencyReporting()
 {
-	// Implement
+	DoPings = true;
 }
 
 void FDriftFlexmatch::StopLatencyReporting()
 {
-	// Implement
+	DoPings = false;
 }
 
 TMap<FString, int16> FDriftFlexmatch::GetLatencyAverages()
--- Drift/Private/DriftFlexmatch.h
+++ Drift/Private/DriftFlexmatch.h
@@ -8,14 +8,21 @@
 DECLARE_LOG_CATEGORY_EXTERN(LogDriftMatchmaking, Log, All);
 
 
-class FDriftFlexmatch : public IDriftMatchmaker, public FSelfRegisteringExec
+class FDriftFlexmatch : public IDriftMatchmaker, FTickableGameObject
 {
 public:
-	FDriftFlexmatch(TSharedPtr<IDriftMessageQueue> MessageQueue);
+	FDriftFlexmatch(TSharedPtr<IDriftMessageQueue> MessageQueue_);
 	~FDriftFlexmatch() override;
+
 	void SetRequestManager(TSharedPtr<JsonRequestManager> RootRequestManager);
 	void SetEndpoint(const FString& MatchmakingUrl);
 
+	// FTickableGameObject overrides
+	void Tick(float DeltaTime) override;
+	bool IsTickable() const override;
+	TStatId GetStatId() const override;
+
+	// IDriftMatchmaker overrides
 	void StartLatencyReporting() override;
 	void StopLatencyReporting() override;
 	TMap<FString, int16> GetLatencyAverages() override;
@@ -36,6 +43,7 @@ public:
 
 private:
 	void HandleMatchmakingEvent(const FMessageQueueEntry& Message);
+	void ReportLatencies();
 
 	TSharedPtr<JsonRequestManager> RequestManager;
 	TSharedPtr<IDriftMessageQueue> MessageQueue;
@@ -48,4 +56,12 @@ private:
 	FPotentialMatchCreatedDelegate OnPotentialMatchCreatedDelegate;
 	FAcceptMatchDelegate OnAcceptMatchDelegate;
 	FMatchmakingSuccessDelegate OnMatchmakingSuccessDelegate;
+
+	// Latency measuring/reporting
+	bool DoPings = false;
+	const float PingInterval = 7.0;
+	float TimeToPing = 0;
+	const FString PingUrlTemplate = TEXT("https://gamelift.{0}.amazonaws.com");
+	const TArray<FString> PingRegions{"eu-west-1"};
+
 };
